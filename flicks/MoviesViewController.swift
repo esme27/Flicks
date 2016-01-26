3d3081320e8ebae11880ccd751ef313a9405a52b
Esme <Esme@Esmes-MBP.att.net>
2016-01-25 22:11:55 -0800

    Adding a detail table view of movies

--- flicks/MoviesViewController.swift
+++ flicks/MoviesViewController.swift
@@ -19,6 +19,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     var refreshControl = UIRefreshControl()
     var movies: [NSDictionary]?
+
     
     
     
@@ -129,22 +130,32 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies! [indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
-        let posterPath = movie["poster_path"] as! String
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        
+        //if let posterPath = movie["poster_path"] as! String {
         
         let baseUrl = "http://image.tmdb.org/t/p/w500"
         
+        
+        if let posterPath = movie["poster_path"] as? String {
+
         let imageUrl = NSURL(string: baseUrl + posterPath)
-        
-        
-        cell.titleLabel.text = title
-        cell.overviewLabel.text = overview 
         cell.posterView.setImageWithURL(imageUrl!)
+
+        
+        //cell.titleLabel.text = title
+        //cell.overviewLabel.text = overview
+        //cell.posterView.setImageWithURL(imageUrl!)
         
         
-        
+        }
         
       
-        print("row \(indexPath.row)")
+       //print("row \(indexPath.row)")
+            
+        
         
         return cell
         
@@ -153,14 +164,22 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
 
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+                   let cell = sender as! UITableViewCell
+            let indexPath = tableView.indexPathForCell(cell)
+            let movie = movies! [indexPath!.row]
+        
+        let detailViewController = segue.destinationViewController as! DetailViewController
+        detailViewController.movie = movie 
+        
+            
+            print("prepare For Segue called")
     }
-    */
+    
 
 }
